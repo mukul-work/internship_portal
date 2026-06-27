e5b2254ffb0e481a7ffe2e05e69c3200b8030a30
Mukul <mukulkashyap.business@gmail.com>
2026-06-27 22:07:15 +0530

    Test PRISMA Crud

--- scripts/prisma_test.ts
+++ scripts/prisma_test.ts
@@ -1,18 +1,11 @@
-import {prisma} from "../lib/prisma"
-async function test(){
-    const st = await prisma.student.create({
-        data:{
-             name: "Mukul",
-             universityRollNo: "202401100200205",
-             email: "mukul.kashyap.work@gmail.com",
-             contact: "9520856100",
-             gender: "Male",
-             session: "2028",
-             semester: "4",
-             section: "C",
-        }
-    })
-    console.log("Created Student:", st)
+import { prisma } from "../lib/prisma";
+async function test() {
+  const st = await prisma.student.delete({
+    where: {
+      universityRollNo: "202401100200205",
+    },
+  });
+  console.log("Student deleted:", st);
 }
 
 test()
