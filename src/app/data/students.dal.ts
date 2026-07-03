e8bcd09b457a3df1c424dd20d4996de2e3a150ed
Mukul <mukulkashyap.business@gmail.com>
2026-07-04 00:22:59 +0530

    Add DAL layer for fetching student internship data

--- src/app/data/students.dal.ts
+++ src/app/data/students.dal.ts
@@ -1,2 +1,13 @@
-import { Student } from "@/types/student.type";
-export async function 
+import { studentInternshipDTO } from "@/types/student-internship.dto";
+
+export async function getStudentInternshipData(): Promise<
+  studentInternshipDTO[]
+> {
+  const response = await fetch("/api/students");
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch Student Internship Data");
+  }
+
+  return response.json();
+}
