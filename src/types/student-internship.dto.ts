717c00ed3244623b3556260fe7974d828277f6d2
Mukul <mukulkashyap.business@gmail.com>
2026-07-03 23:52:04 +0530

    add DTO for student-internships

---
+++ src/types/student-internship.dto.ts
@@ -0,0 +1,33 @@
+export interface InternshipDTO {
+  internshipId: number;
+  studentId: number;
+  sourceOfInternship: string;
+  internshipType: string;
+  role: string;
+  duration: string;
+  startDate: string;
+  endDate: string;
+  stipend: string;
+  stipendAmount?: string;
+  ppo: string;
+  ppoProof?: string;
+  organisationName: string;
+  organisationAddress: string;
+  status: string;
+  completionProof?: string;
+  student: string;
+}
+
+export interface studentInternshipDTO {
+  studentId: number;
+  studentName: string;
+  studentContact: string;
+  studentEmail: string;
+  studentGender: string;
+  studentInternship: string;
+  studentSection: string;
+  studentSemester: string;
+  studentSession: string;
+  studentUniversityRollNo: string;
+  internship: InternshipDTO[];
+}
