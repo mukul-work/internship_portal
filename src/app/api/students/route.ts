d36c0bf423357d3fa42bae2033baed4c8088a4ed
Mukul <mukulkashyap.business@gmail.com>
2026-07-05 22:50:59 +0530

    Add pagination to api/students route

--- src/app/api/students/route.ts
+++ src/app/api/students/route.ts
@@ -1,42 +1,38 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 import { studentInternshipDTO } from "@/types/student-internship.dto";
+import parseQuery from "@/lib/student-internship-query/parseQuery";
+import { StudentQuery } from "@/types/studentQuery.type";
+import { studentQuerySchema } from "@/types/studentQuery.type";
+import { studentSelect } from "@/lib/student-internship-query/studentSelect";
+import { buildStudentOrderBy } from "@/lib/student-internship-query/buildStudentSortBy";
+import { buildStudentWhere } from "@/lib/student-internship-query/buildStudentWhere";
 export const GET = async (request: Request) => {
   try {
-    const students = await prisma.student.findMany({
-      select: {
-        studentId: true,
-        studentName: true,
-        studentContact: true,
-        studentEmail: true,
-        studentGender: true,
-        studentSection: true,
-        studentSemester: true,
-        studentSession: true,
-        studentUniversityRollNo: true,
-        studentInternships: {
-          select: {
-            internshipId: true,
-            studentId: true,
-            sourceOfInternship: true,
-            internshipType: true,
-            role: true,
-            duration: true,
-            startDate: true,
-            endDate: true,
-            stipend: true,
-            stipendAmount: true,
-            ppo: true,
-            ppoProof: true,
-            organisationName: true,
-            organisationAddress: true,
-            status: true,
-            completionProof: true,
-          },
-        },
-      },
-    });
-    const response: studentInternshipDTO[] = students;
+    const { searchParams } = new URL(request.url);
+    const query: StudentQuery = parseQuery(searchParams);
+    const where = buildStudentWhere(query);
+    const orderBy = buildStudentOrderBy(query);
+    const skip = (Number(query.page) - 1) * Number(query.pageSize);
+    const take = Number(query.pageSize);
+
+    const [students, total] = await prisma.$transaction([
+      prisma.student.findMany({
+        where,
+        orderBy,
+        skip,
+        take,
+        select: studentSelect,
+      }),
+      prisma.student.count({ where }),
+    ]);
+    const response = {
+      data: students,
+      total,
+      page: query.page,
+      pageSize: query.pageSize,
+      totalPages: Math.ceil(total / Number(query.pageSize)),
+    };
     return NextResponse.json(response, { status: 200 });
   } catch (error) {
     throw error;
