66e47811ebbcc03dbdc8530aa7db470881fabb3a
Mukul <mukulkashyap.business@gmail.com>
2026-07-05 22:50:25 +0530

    Add a separate file for building the select clause

---
+++ src/lib/student-internship-query/studentSelect.ts
@@ -0,0 +1,33 @@
+import { Prisma } from "@/generated/prisma/client";
+
+export const studentSelect = {
+  studentId: true,
+  studentName: true,
+  studentUniversityRollNo: true,
+  studentEmail: true,
+  studentContact: true,
+  studentGender: true,
+  studentSession: true,
+  studentSemester: true,
+  studentSection: true,
+  studentInternships: {
+    select: {
+      internshipId: true,
+      studentId: true,
+      sourceOfInternship: true,
+      internshipType: true,
+      role: true,
+      duration: true,
+      startDate: true,
+      endDate: true,
+      stipend: true,
+      stipendAmount: true,
+      ppo: true,
+      ppoProof: true,
+      organisationName: true,
+      organisationAddress: true,
+      status: true,
+      completionProof: true,
+    },
+  },
+} satisfies Prisma.StudentSelect;
