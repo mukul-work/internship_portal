4bcbfec97d986daf12e86c568096513686d67699
Mukul <mukulkashyap.business@gmail.com>
2026-07-06 14:24:15 +0530

    add validation steps for the internship POST request

---
+++ src/lib/internship-input/validateInternshipRequest.ts
@@ -0,0 +1,38 @@
+import { internshipInputSchema } from "@/lib/validations/internshipInput";
+import { ValidateInternshipResult } from "@/types/validateInternshipRequestResult.type";
+import { prisma } from "@/lib/prisma";
+export async function validateInternshipRequest(
+  request: Request,
+): Promise<ValidateInternshipResult> {
+  const body = await request.json();
+
+  const result = internshipInputSchema.safeParse(body);
+
+  if (!result.success) {
+    return {
+      success: false,
+      status: 400,
+      message: "Invalid Request",
+    };
+  }
+
+  const student = await prisma.student.findUnique({
+    where: {
+      studentId: result.data.studentId,
+    },
+  });
+
+  if (!student) {
+    return {
+      success: false,
+      status: 404,
+      message: "Student not found",
+    };
+  }
+
+  return {
+    success: true,
+    status: 200,
+    data: result.data,
+  };
+}
