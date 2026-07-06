7465748234865f878366ba9ef07e45fecdeb6486
Mukul <mukulkashyap.business@gmail.com>
2026-07-06 23:57:10 +0530

    add GET and DELETE methods

--- src/app/api/internships/[internshipId]/route.ts
+++ src/app/api/internships/[internshipId]/route.ts
@@ -5,6 +5,64 @@ import { validateInternshipRequest } from "@/lib/internship-input/validateIntern
 import { Prisma } from "@/generated/prisma/client";
 import { validateInternship } from "@/lib/validations/internshipValidation";
 
+export async function GET({ params }: { params: Promise<{ internshipId: string }> }) {
+
+    try {
+        // Session validation
+        const sessionValidationResult = await validateSession();
+
+        if (!sessionValidationResult.data) {
+            return NextResponse.json(
+                { message: sessionValidationResult.message },
+                { status: sessionValidationResult.status },
+            );
+        }
+
+        // Internship Validation
+        const { internshipId } = await params;
+        const id = Number(internshipId)
+        const internshipValidationResult = await validateInternship(id, sessionValidationResult.data.studentId)
+
+        if (!internshipValidationResult.success) {
+            return NextResponse.json({
+                message: internshipValidationResult.message
+            }, {
+                status: internshipValidationResult.status
+            })
+        }
+
+
+
+        // Get Internship
+        return NextResponse.json(
+            { message: "Internship fetched successfully", data: internshipValidationResult.data },
+            { status: 200 },
+        );
+
+    } catch (error) {
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            return NextResponse.json(
+                {
+                    message: error.message,
+                },
+                {
+                    status: 400,
+                },
+            );
+        }
+
+        console.error(error);
+
+        return NextResponse.json(
+            {
+                message: "Internal Server Error",
+            },
+            {
+                status: 500,
+            },
+        );
+    }
+}
 
 export async function PATCH(
     request: Request,
@@ -54,7 +112,7 @@ export async function PATCH(
         });
 
         return NextResponse.json(
-            { message: "Intenrship updated successfully", data: updated },
+            { message: "Internship updated successfully", data: updated },
             { status: 200 },
         );
 
