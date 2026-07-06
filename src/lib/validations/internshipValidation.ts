0234a392f5002565a51624cdab48a8dc1f8d518d
Mukul <mukulkashyap.business@gmail.com>
2026-07-06 23:43:25 +0530

    add internship validation

---
+++ src/lib/validations/internshipValidation.ts
@@ -0,0 +1,30 @@
+import { prisma } from "@/lib/prisma"
+import { success } from "zod";
+
+export async function validateInternship(internshipId: number, studentId: Number) {
+
+
+    if (Number.isNaN(internshipId)) {
+        return { message: "Invalid internship id", status: 400, success: false, data: null };
+    }
+
+    const internship = await prisma.internship.findUnique({
+        where: { internshipId },
+    });
+
+    if (!internship) {
+        return { message: "No internship found", status: 404, success: false, data: null };
+    }
+
+    if (internship.studentId !== studentId) {
+        return { message: "Forbidden", status: 403, success: false, data: null };
+    }
+
+    return {
+        mesage: "Internship is valid",
+        status: 200,
+        success: true,
+        data: internship
+    }
+}
+
