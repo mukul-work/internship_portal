133d391435c0af879da6ee1b37a86cb4082d26c3
Mukul <mukulkashyap.business@gmail.com>
2026-07-07 23:55:44 +0530

    add 'pages' property for redirecting to login page

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -58,7 +58,7 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
-
+  pages: { signIn: "/auth/admin/login" },
   session: {
     strategy: "jwt",
   },
