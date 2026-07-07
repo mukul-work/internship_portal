48c61d9c1cd6323e960517939a2787fad0c9aa59
Mukul <mukulkashyap.business@gmail.com>
2026-07-07 23:28:24 +0530

    update route structure

---
+++ src/app/api/student/internships/[internshipId]/route.ts
@@ -0,0 +1,233 @@
+import { NextResponse } from "next/server";
+import { validateStudentSession } from "@/lib/validations/studentSessionValidation";
+import { prisma } from "@/lib/prisma";
+import { validateInternshipRequest } from "@/lib/internship-input/validateInternshipRequest";
+import { Prisma } from "@/generated/prisma/client";
+import { validateInternship } from "@/lib/validations/internshipValidation";
+import { success } from "zod";
+
+export async function GET(
+  request: Request,
+  { params }: { params: Promise<{ internshipId: string }> },
+) {
+  try {
+    // Session validation
+    const studentValidationResult = await validateStudentSession();
+
+    if (!studentValidationResult.data) {
+      return NextResponse.json(
+        {
+          success: studentValidationResult.success,
+          message: studentValidationResult.message,
+        },
+        { status: studentValidationResult.status },
+      );
+    }
+
+    // Internship Validation
+    const { internshipId } = await params;
+    const id = Number(internshipId);
+    const internshipValidationResult = await validateInternship(
+      id,
+      studentValidationResult.data.studentId,
+    );
+
+    if (!internshipValidationResult.success) {
+      return NextResponse.json(
+        {
+          success: internshipValidationResult.success,
+          message: internshipValidationResult.message,
+        },
+        {
+          status: internshipValidationResult.status,
+        },
+      );
+    }
+
+    // Get Internship
+    return NextResponse.json(
+      {
+        success: true,
+        message: "Internship fetched successfully",
+        data: internshipValidationResult.data,
+      },
+      { status: 200 },
+    );
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      return NextResponse.json(
+        { success: false, message: error.message },
+        {
+          status: 400,
+        },
+      );
+    }
+
+    console.error(error);
+
+    return NextResponse.json(
+      { success: false, message: "Internal Server Error" },
+      {
+        status: 500,
+      },
+    );
+  }
+}
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: Promise<{ internshipId: string }> },
+) {
+  try {
+    // Session validation
+    const studentValidationResult = await validateStudentSession();
+
+    if (!studentValidationResult.data) {
+      return NextResponse.json(
+        {
+          success: studentValidationResult.success,
+          message: studentValidationResult.message,
+        },
+        { status: studentValidationResult.status },
+      );
+    }
+
+    // Internship Validation
+    const { internshipId } = await params;
+    const id = Number(internshipId);
+    const internshipValidationResult = await validateInternship(
+      id,
+      studentValidationResult.data.studentId,
+    );
+
+    if (!internshipValidationResult.success) {
+      return NextResponse.json(
+        {
+          success: internshipValidationResult.success,
+          message: internshipValidationResult.message,
+        },
+        {
+          status: internshipValidationResult.status,
+        },
+      );
+    }
+
+    // Request Validation
+    const result = await validateInternshipRequest(request);
+
+    if (!result.success) {
+      return NextResponse.json(
+        { success: result.success, message: result.message },
+        { status: result.status },
+      );
+    }
+
+    // Update Internship
+    const updated = await prisma.internship.update({
+      where: {
+        internshipId: id,
+      },
+      data: {
+        ...result.data,
+        studentId: studentValidationResult.data.studentId,
+      },
+    });
+
+    return NextResponse.json(
+      {
+        success: true,
+        message: "Internship updated successfully",
+        data: updated,
+      },
+      { status: 200 },
+    );
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      return NextResponse.json(
+        { success: false, message: error.message },
+        {
+          status: 400,
+        },
+      );
+    }
+
+    console.error(error);
+
+    return NextResponse.json(
+      { success: true, message: "Internal Server Error" },
+      {
+        status: 500,
+      },
+    );
+  }
+}
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: Promise<{ internshipId: string }> },
+) {
+  try {
+    // Session validation
+    const studentValidationResult = await validateStudentSession();
+
+    if (!studentValidationResult.data) {
+      return NextResponse.json(
+        {
+          success: studentValidationResult.success,
+          message: studentValidationResult.message,
+        },
+        { status: studentValidationResult.status },
+      );
+    }
+
+    // Internship Validation
+    const { internshipId } = await params;
+    const id = Number(internshipId);
+    const internshipValidationResult = await validateInternship(
+      id,
+      studentValidationResult.data.studentId,
+    );
+
+    if (!internshipValidationResult.success) {
+      return NextResponse.json(
+        {
+          success: internshipValidationResult.success,
+          message: internshipValidationResult.message,
+        },
+        {
+          status: internshipValidationResult.status,
+        },
+      );
+    }
+
+    // Delete Internship
+    await prisma.internship.delete({
+      where: {
+        internshipId: id,
+      },
+    });
+
+    return NextResponse.json(
+      { success: true, message: "Internship Deleted successfully" },
+      { status: 200 },
+    );
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      return NextResponse.json(
+        { success: false, message: error.message },
+        {
+          status: 400,
+        },
+      );
+    }
+
+    console.error(error);
+
+    return NextResponse.json(
+      { success: false, message: "Internal Server Error" },
+      {
+        status: 500,
+      },
+    );
+  }
+}
---
+++ src/app/api/student/internships/route.ts
@@ -0,0 +1,69 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+import { Prisma } from "@/generated/prisma/client";
+import { validateInternshipRequest } from "@/lib/internship-input/validateInternshipRequest";
+import { validateStudentSession } from "@/lib/validations/studentSessionValidation";
+import { success } from "zod";
+export const POST = async (request: Request) => {
+  try {
+    // Session Validation
+    const studentValidationResult = await validateStudentSession();
+
+    if (!studentValidationResult.data) {
+      return NextResponse.json(
+        {
+          success: studentValidationResult.success,
+          message: studentValidationResult.message,
+        },
+        { status: studentValidationResult.status },
+      );
+    }
+
+    // Request Validation
+    const result = await validateInternshipRequest(request);
+
+    if (!result.success) {
+      return NextResponse.json(
+        { success: result.success, message: result.message },
+        { status: result.status },
+      );
+    }
+
+    const internship = await prisma.internship.create({
+      data: {
+        ...result.data,
+        studentId: studentValidationResult.data.studentId,
+      },
+    });
+
+    if (!internship) {
+      return NextResponse.json(
+        { success: false, message: "Failed to create internship" },
+        { status: 500 },
+      );
+    }
+
+    return NextResponse.json(
+      { success: true, message: "Internship created successfully" },
+      { status: 201 },
+    );
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      return NextResponse.json(
+        { success: false, message: error.message },
+        {
+          status: 400,
+        },
+      );
+    }
+
+    console.error(error);
+
+    return NextResponse.json(
+      { success: false, message: "Internal Server Error" },
+      {
+        status: 500,
+      },
+    );
+  }
+};
