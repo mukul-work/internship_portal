badabaee80c8ed1db560a6f93d73929796d10e16
Mukul <mukulkashyap.business@gmail.com>
2026-07-07 23:30:22 +0530

    add google auth provider

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,7 @@
 import { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { prisma } from "@/lib/prisma";
+import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -38,9 +39,11 @@ export const authOptions: NextAuthOptions = {
             password === admin.adminPassword ? true : false;
           if (isPasswordCorrect) {
             return {
-              id: admin.adminId,
+              adminId: admin.adminId.toString(),
+              studentId: undefined,
               email: admin.adminEmail,
               name: admin.adminName,
+              role: "ADMIN",
             };
           } else {
             throw new Error("Incorrect Password");
@@ -50,29 +53,68 @@ export const authOptions: NextAuthOptions = {
         }
       },
     }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
   ],
-  // pages: {
-  //   signIn: "/auth/signin",
-  // },
+
   session: {
     strategy: "jwt",
   },
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
+    async signIn({ user, account }) {
+      if (account?.provider === "google") {
+        if (!user.email?.endsWith("kiet@edu")) {
+          return false;
+        }
+        const student = await prisma.student.findUnique({
+          where: {
+            studentEmail: user.email,
+          },
+        });
+
+        return !!student;
+      }
+      return true;
+    },
+
     async session({ session, token }) {
       if (token) {
-        session.user.id = token._id?.toString();
+        session.user.adminId = token.adminId?.toString();
+        session.user.studentId = token.studentId?.toString();
         session.user.email = token.email?.toString();
         session.user.name = token.name?.toString();
+        session.user.role = token.role?.toString();
       }
       return session;
     },
-    async jwt({ token, user }) {
-      if (user) {
-        token._id = user.id?.toString();
-        token.email = user.email?.toString();
-        token.name = user.name?.toString();
+    async jwt({ token, user, account }) {
+      if (account?.provider === "google" && user.email) {
+        const student = await prisma.student.findUnique({
+          where: {
+            studentEmail: user.email,
+          },
+        });
+
+        if (student) {
+          token.studentId = student.studentId.toString();
+          token.adminId = undefined;
+          token.name = student.studentName;
+          token.email = student.studentEmail;
+          token.role = "STUDENT";
+        }
       }
+
+      if (account?.provider === "credentials" && user) {
+        token.adminId = user.adminId;
+        token.studentId = user.studentId;
+        token.name = user.name;
+        token.email = user.email;
+        token.role = user.role;
+      }
+
       return token;
     },
   },
