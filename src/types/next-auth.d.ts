ad2648927b8ed84acefa32aab37886900d1d9e12
Mukul <mukulkashyap.business@gmail.com>
2026-07-07 23:27:23 +0530

    update user, session and jwt type

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,10 +1,26 @@
 import "next-auth";
-import { DefaultSession } from "next-auth";
+import { DefaultSession, DefaultUser } from "next-auth";
 
 declare module "next-auth" {
   interface Session {
     user: {
-      id?: string;
+      adminId?: string;
+      studentId?: string;
+      role?: string;
     } & DefaultSession["user"];
+  }
+
+  interface User {
+    adminId?: string;
+    studentId?: string;
+    role?: string;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    adminId?: string;
+    studentId?: string;
+    role?: string;
   }
 }
