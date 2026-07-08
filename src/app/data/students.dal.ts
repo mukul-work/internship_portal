ad5d77d9aa3c63e2cb44135a54a797158eae3057
Mukul <mukulkashyap.business@gmail.com>
2026-07-08 21:31:31 +0530

    add new functions in DAL and service layer

--- src/app/data/students.dal.ts
+++ src/app/data/students.dal.ts
@@ -1,7 +1,7 @@
 import { studentInternshipDTO } from "@/types/student-internship.dto";
 import { cookies } from "next/headers";
 
-export async function getStudentInternshipData(): Promise<
+export async function getAllStudentInternshipData(): Promise<
   studentInternshipDTO[]
 > {
   const cookieStore = await cookies();
@@ -13,6 +13,17 @@ export async function getStudentInternshipData(): Promise<
       },
     },
   );
+
+  return response.json();
+}
+
+export async function getStudentInternshipDataUsingSession(): Promise<studentInternshipDTO> {
+  const cookieStore = await cookies();
+  const response = await fetch(`${process.env.NEXTAUTH_URL}/api/student`, {
+    headers: {
+      Cookie: cookieStore.toString(),
+    },
+  });
 
   return response.json();
 }
--- src/app/data/students.service.ts
+++ src/app/data/students.service.ts
@@ -1,4 +1,7 @@
-import { getStudentInternshipData } from "./students.dal";
+import {
+  getAllStudentInternshipData,
+  getStudentInternshipDataUsingSession,
+} from "./students.dal";
 import { studentInternshipDTO } from "@/types/student-internship.dto";
 
 interface ServiceResponse<T> {
@@ -7,11 +10,11 @@ interface ServiceResponse<T> {
   data: T | null;
 }
 
-export async function fetchStudentInternshipData(): Promise<
+export async function fetchAllStudentsInternshipData(): Promise<
   ServiceResponse<studentInternshipDTO[]>
 > {
   try {
-    const response = await getStudentInternshipData();
+    const response = await getAllStudentInternshipData();
     return {
       success: true,
       message: "Student Internship Data fetched successfully",
@@ -22,6 +25,26 @@ export async function fetchStudentInternshipData(): Promise<
     return {
       success: false,
       message: "Failed to fetch student internship data",
+      data: null,
+    };
+  }
+}
+
+export async function fetchStudentInternshipDataUsingSession(): Promise<
+  ServiceResponse<studentInternshipDTO>
+> {
+  try {
+    const response = await getStudentInternshipDataUsingSession();
+    return {
+      success: true,
+      message: "Student Internship Data fetched successfully by ID",
+      data: response,
+    };
+  } catch (err) {
+    console.error("Error: ", err);
+    return {
+      success: false,
+      message: "Failed to fetch student internship data by ID",
       data: null,
     };
   }
