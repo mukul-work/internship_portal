5483190c8d8838539391eed2bc2063d4e14b067f
Mukul <mukulkashyap.business@gmail.com>
2026-07-09 12:32:04 +0530

    add action layer for student updation

---
+++ src/app/action/students.action.ts
@@ -0,0 +1,29 @@
+import { patchStudentDataForStudent } from "../data/students.dal";
+import { studentInternshipDTO } from "@/types/student-internship.dto";
+import { studentPATCH } from "@/types/student.PATCH";
+
+interface Response {
+  success: boolean;
+  message: string;
+  data: studentInternshipDTO | studentInternshipDTO[] | null;
+}
+
+export async function fetchStudentInternshipDataUsingSession(
+  data: studentPATCH,
+): Promise<Response> {
+  try {
+    const response = await patchStudentDataForStudent(data);
+    return {
+      success: response.success,
+      message: response.message,
+      data: response.data,
+    };
+  } catch (err) {
+    console.error("Error: ", err);
+    return {
+      success: false,
+      message: "Failed to update student data",
+      data: null,
+    };
+  }
+}
