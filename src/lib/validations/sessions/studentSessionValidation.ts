8f899ce2a5e9fc157b80f8e397de23855c1cc1ae
Mukul <mukulkashyap.business@gmail.com>
2026-07-09 15:56:56 +0530

    add a condition to check whether the studentId in the request is same as the studentId fetched from the DB

--- src/lib/validations/sessions/studentSessionValidation.ts
+++ src/lib/validations/sessions/studentSessionValidation.ts
@@ -1,8 +1,9 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "../../auth";
 import { prisma } from "@/lib/prisma";
+import { studentInputSchema } from "../student-input/studentInput";
 
-export async function validateStudentSession() {
+export async function validateStudentSession(request: Request) {
   const session = await getServerSession(authOptions);
 
   if (!(session?.user?.role === "STUDENT")) {
@@ -11,6 +12,18 @@ export async function validateStudentSession() {
 
   if (!session?.user?.email) {
     return { message: "Email not found", status: 400, data: null };
+  }
+
+  const body = await request.json();
+  const result = studentInputSchema.safeParse(body);
+
+  if (!result.success) {
+    return {
+      success: false,
+      status: 400,
+      message: "Invalid Request",
+      data: null,
+    };
   }
 
   const student = await prisma.student.findUnique({
@@ -29,7 +42,14 @@ export async function validateStudentSession() {
       data: null,
     };
   }
-
+  if (result.data.studentId !== student.studentId) {
+    return {
+      success: false,
+      message: "Invalid StudentId",
+      status: 400,
+      data: null,
+    };
+  }
   return {
     success: true,
     message: "Student authorised and found",
