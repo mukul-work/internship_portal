03682dd5c5cce49787711f6db4a877bffb58e8e9
Mukul <mukulkashyap.business@gmail.com>
2026-07-09 12:28:35 +0530

    add interface for student updation data

---
+++ src/types/student.PATCH.ts
@@ -0,0 +1,11 @@
+export interface studentPATCH {
+  studentId: number;
+  studentName: string;
+  studentContact: string;
+  studentEmail: string;
+  studentGender: string;
+  studentSection: string;
+  studentSemester: string;
+  studentSession: string;
+  studentUniversityRollNo: string;
+}
