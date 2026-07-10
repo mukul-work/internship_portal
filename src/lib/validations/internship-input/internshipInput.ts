b2b343350166801e09319433101e2e1a45109d58
Mukul <mukulkashyap.business@gmail.com>
2026-07-10 13:28:15 +0530

    add zod validation for PATCH requests

--- src/lib/validations/internship-input/internshipInput.ts
+++ src/lib/validations/internship-input/internshipInput.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-export const internshipInputSchema = z.object({
+export const internshipInputSchemaPOST = z.object({
   studentId: z.number().int().positive(),
   sourceOfInternship: z.string().min(1),
   internshipType: z.string().min(1),
@@ -18,4 +18,25 @@ export const internshipInputSchema = z.object({
   completionProof: z.string().optional(),
 });
 
-export type InternshipInput = z.infer<typeof internshipInputSchema>;
+export type InternshipInputPOST = z.infer<typeof internshipInputSchemaPOST>;
+
+export const internshipInputSchemaPATCH = z.object({
+  internshpiId: z.number().int().positive(),
+  studentId: z.number().int().positive(),
+  sourceOfInternship: z.string().min(1),
+  internshipType: z.string().min(1),
+  role: z.string().min(1),
+  duration: z.string().min(1),
+  startDate: z.string(),
+  endDate: z.string(),
+  stipend: z.boolean(),
+  stipendAmount: z.string().optional(),
+  ppo: z.boolean(),
+  ppoProof: z.string().optional(),
+  organisationName: z.string().min(1),
+  organisationAddress: z.string().min(1),
+  status: z.string().min(1),
+  completionProof: z.string().optional(),
+});
+
+export type InternshipInputPATCH = z.infer<typeof internshipInputSchemaPATCH>;
