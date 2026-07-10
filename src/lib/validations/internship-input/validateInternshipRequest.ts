c1c6d3971a0652bc13e7e6284a4b8f314af15c22
Mukul <mukulkashyap.business@gmail.com>
2026-07-10 13:28:57 +0530

    create separate validation fucntions for POST and PATCH requestse

--- src/lib/validations/internship-input/validateInternshipRequest.ts
+++ src/lib/validations/internship-input/validateInternshipRequest.ts
@@ -1,28 +1,24 @@
-import { internshipInputSchema } from "@/lib/validations//internship-input/internshipInput";
+import {
+  internshipInputSchemaPATCH,
+  internshipInputSchemaPOST,
+} from "@/lib/validations//internship-input/internshipInput";
 import { prisma } from "@/lib/prisma";
-import { InternshipInput } from "./internshipInput";
+import { InternshipInputPOST, InternshipInputPATCH } from "./internshipInput";
 
-type internshipValidationResult =
-  | {
-      success: true;
-      status: 200;
-      message: string;
-      data: InternshipInput;
-    }
-  | {
-      success: false;
-      status: number;
-      message: string;
-      data: null;
-    };
+type internshipValidationResult = {
+  success: boolean;
+  status: number;
+  message: string;
+  data: InternshipInputPOST | InternshipInputPATCH | null;
+};
 
-export async function validateInternshipRequest(
+export async function validateInternshipPOSTRequest(
   request: Request,
   studentId: number,
 ): Promise<internshipValidationResult> {
   const body = await request.json();
 
-  const result = internshipInputSchema.safeParse(body);
+  const result = internshipInputSchemaPOST.safeParse(body);
 
   if (!result.success) {
     return {
@@ -37,6 +33,64 @@ export async function validateInternshipRequest(
     return {
       success: false,
       message: "Invalid internship Id",
+      data: null,
+      status: 400,
+    };
+  }
+
+  const student = await prisma.student.findUnique({
+    where: {
+      studentId: result.data.studentId,
+    },
+  });
+
+  if (!student) {
+    return {
+      success: false,
+      status: 404,
+      message: "Student not found",
+      data: null,
+    };
+  }
+
+  return {
+    success: true,
+    status: 200,
+    data: result.data,
+    message: "Internship Validated",
+  };
+}
+
+export async function validateInternshipPATCHRequest(
+  request: Request,
+  internshipId: number,
+  studentId: number,
+): Promise<internshipValidationResult> {
+  const body = await request.json();
+
+  const result = internshipInputSchemaPATCH.safeParse(body);
+
+  if (!result.success) {
+    return {
+      success: false,
+      status: 400,
+      message: "Invalid Request",
+      data: null,
+    };
+  }
+
+  if (result.data.studentId !== studentId) {
+    return {
+      success: false,
+      message: "Invalid student Id",
+      data: null,
+      status: 400,
+    };
+  }
+  if (result.data.internshpiId !== internshipId) {
+    return {
+      success: false,
+      message: "Invalid student Id",
       data: null,
       status: 400,
     };
