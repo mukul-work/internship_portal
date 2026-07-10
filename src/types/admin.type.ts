588d591365156dbaed18f76060018b5a7699bd59
Mukul <mukulkashyap.business@gmail.com>
2026-07-10 11:25:59 +0530

    update field names in the admin type

--- src/types/admin.type.ts
+++ src/types/admin.type.ts
@@ -1,6 +1,6 @@
 export interface Admin {
-  id: number;
-  email: string;
-  password: string;
-  name: string;
+  adminId: number;
+  adminEmail: string;
+  adminPassword: string;
+  adminName: string;
 }
