0f1686582a9776064fca86eed7bd3764248414e0
Mukul <mukulkashyap.business@gmail.com>
2026-07-12 20:35:09 +0530

    add a condition to check whether data is being sent from the backend or not

--- src/app/students/details/page.tsx
+++ src/app/students/details/page.tsx
@@ -2,14 +2,20 @@ import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 import StudentDetails from "./_components/StudentDetails";
 import { fetchStudentDataForStudent } from "@/app/data/students.service";
-import { StudentDetailsError } from "./_components/error/StudentError";
+import {
+  StudentDataError,
+  StudentAuthError,
+} from "../_components/error/StudentError";
 
 export default async function StudentDetailsPage() {
   const session = await getServerSession(authOptions);
   if (session?.user?.role !== "STUDENT") {
-    return <StudentDetailsError />;
+    return <StudentAuthError />;
   }
   const response = await fetchStudentDataForStudent();
+  if (!response.data) {
+    return <StudentDataError />;
+  }
   console.log("Student response: ", response);
   return <StudentDetails data={response.data} />;
 }
