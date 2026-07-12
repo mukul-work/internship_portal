cafcc22a986f1c70b49fd24b56f5c5041b34c5f6
Mukul <mukulkashyap.business@gmail.com>
2026-07-12 20:24:16 +0530

    rename Internship-Module to students

--- src/app/Internship-Module/page.tsx
+++
@@ -1,7 +0,0 @@
-import { fetchStudentInternshipDataUsingSession } from "../data/students.service";
-
-export default async function Page() {
-  const result = await fetchStudentInternshipDataUsingSession();
-  console.log("Fetched Data: ", JSON.stringify(result, null, 2));
-  return <div>Hello There</div>;
-}
