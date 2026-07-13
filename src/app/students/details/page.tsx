44c9bd084b4c4d90b2e90b7d732215cf56ff97e6
Mukul <mukulkashyap.business@gmail.com>
2026-07-13 18:09:53 +0530

    remove session check bcause middleware already handles it

--- src/app/students/details/page.tsx
+++ src/app/students/details/page.tsx
@@ -1,19 +1,10 @@
-import { authOptions } from "@/lib/auth";
-import { getServerSession } from "next-auth";
 import StudentDetails from "./_components/StudentDetails";
 import { fetchStudentDataForStudent } from "@/app/data/students.service";
-import {
-  StudentDataError,
-  StudentAuthError,
-} from "../_components/error/StudentError";
+import { StudentDataError } from "../_components/error/StudentError";
 
 export default async function StudentDetailsPage() {
-  const session = await getServerSession(authOptions);
-  if (session?.user?.role !== "STUDENT") {
-    return <StudentAuthError />;
-  }
   const response = await fetchStudentDataForStudent();
-  if (!response.data) {
+  if (!response.success) {
     return <StudentDataError />;
   }
   console.log("Student response: ", response);
