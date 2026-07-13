628b62ec76964532f3380afc1670ad605a20885d
Mukul <mukulkashyap.business@gmail.com>
2026-07-13 20:06:33 +0530

    add DAL functions for GET, PATCH and POST

---
+++ src/types/internships/internship.GET.ts
@@ -0,0 +1,17 @@
+export interface internshipGET {
+  internshipId: number;
+  studentId: number;
+  sourceOfInternship: string;
+  internshipType: string;
+  role: string;
+  startDate: string;
+  endDate: string;
+  stipend: boolean;
+  stipendAmount?: string;
+  ppo: boolean;
+  ppoProof?: string;
+  organisationName: string;
+  organisationAddress: string;
+  status: "completed" | "inProgress";
+  completionProof?: string;
+}
