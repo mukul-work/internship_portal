5bd7860713597cde8768f8a744f8b439d97da784
Mukul <mukulkashyap.business@gmail.com>
2026-07-13 17:19:26 +0530

    Add types for student profile

--- src/types/student-internship.dto.ts
+++ src/types/student-internship.dto.ts
@@ -28,3 +28,15 @@ export interface studentInternshipDTO {
   studentUniversityRollNo: string;
   studentInternships?: InternshipDTO[];
 }
+
+export interface studentProfileDTO {
+  studentId: number;
+  studentName: string;
+  studentContact: string;
+  studentEmail: string;
+  studentGender: "MALE" | "FEMALE" | "OTHER";
+  studentSection: "A" | "B" | "C" | "D" | "E";
+  studentSemester: "2" | "4" | "6";
+  studentSession: "2027" | "2028" | "2029";
+  studentUniversityRollNo: string;
+}
