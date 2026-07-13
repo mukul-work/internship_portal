39948820f2967e9f7d05a9e56cbd2cb4fef4bb44
Mukul <mukulkashyap.business@gmail.com>
2026-07-13 19:31:33 +0530

    refactor the DAL layer for students

--- src/app/data/students.dal.ts
+++ src/app/data/students.dal.ts
@@ -1,61 +1,44 @@
-import { studentProfileDTO } from "@/types/student-internship.dto";
-import { cookies } from "next/headers";
+import { apiRequest } from "@/lib/apiRequest";
+import { ApiResponse } from "@/types/apiResponse.type";
+import { studentInternshipDTO } from "@/types/student-internship.dto";
+import { studentProfileGET } from "@/types/students/student.GET";
+import { studentProfilePATCH } from "@/types/students/student.PATCH";
 
-export async function getAllStudentsDataForAdmin() {
-  const cookieStore = await cookies();
-  const response = await fetch(
+export function getAllStudentsDataForAdmin(): Promise<
+  ApiResponse<studentInternshipDTO[]>
+> {
+  return apiRequest<studentInternshipDTO[]>(
     `${process.env.NEXTAUTH_URL}/api/admin/students`,
-    {
-      headers: {
-        Cookie: cookieStore.toString(),
-      },
-    },
   );
-
-  return response.json();
 }
 
-export async function getStudentDataForStudent() {
-  const cookieStore = await cookies();
-  const response = await fetch(
+export function getStudentDataForStudent(): Promise<
+  ApiResponse<studentInternshipDTO>
+> {
+  return apiRequest<studentInternshipDTO>(
     `${process.env.NEXTAUTH_URL}/api/students/student`,
-    {
-      headers: {
-        Cookie: cookieStore.toString(),
-      },
-    },
   );
-
-  return response.json();
 }
 
-export async function getStudentProfileForStudent() {
-  const cookieStore = await cookies();
-  const response = await fetch(
+export function getStudentProfileForStudent(): Promise<
+  ApiResponse<studentProfileGET>
+> {
+  return apiRequest<studentProfileGET>(
     `${process.env.NEXTAUTH_URL}/api/students/student/profile`,
-    {
-      headers: {
-        Cookie: cookieStore.toString(),
-      },
-    },
   );
-
-  return response.json();
 }
 
-export async function patchStudentProfileForStudent(data: studentProfileDTO) {
-  const cookieStore = await cookies();
-  const response = await fetch(
+export function patchStudentProfileForStudent(
+  data: studentProfilePATCH,
+): Promise<ApiResponse<studentProfilePATCH>> {
+  return apiRequest<studentProfilePATCH>(
     `${process.env.NEXTAUTH_URL}/api/students/student/profile`,
     {
       method: "PATCH",
       headers: {
-        Cookie: cookieStore.toString(),
         "Content-Type": "application/json",
       },
       body: JSON.stringify(data),
     },
   );
-
-  return response.json();
 }
