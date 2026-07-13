3374a0778136391fda45d8520d1f824f3a822ea0
Mukul <mukulkashyap.business@gmail.com>
2026-07-13 19:55:13 +0530

    define types for POST and PATCH

---
+++ src/types/internships/internship.POST.ts
@@ -0,0 +1,16 @@
+export interface internshipPOST {
+  studentId: number;
+  sourceOfInternship: string;
+  internshipType: string;
+  role: string;
+  startDate: string;
+  endDate: string;
+  stipend: boolean;
+  stipendAmount?: string;
+  ppo: boolean;
+  ppoProof?: string;
+  organisationName: string;
+  organisationAddress: string;
+  status: "completed" | "inProgress";
+  completionProof?: string;
+}
