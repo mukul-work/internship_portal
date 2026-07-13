97c8d834c2420b7309f20a0338621140378ed6a5
Mukul <mukulkashyap.business@gmail.com>
2026-07-13 19:57:19 +0530

    remove the profile type

--- src/types/student-internship.dto.ts
+++ src/types/student-internship.dto.ts
@@ -28,15 +28,3 @@ export interface studentInternshipDTO {
   studentUniversityRollNo: string;
   studentInternships?: InternshipDTO[];
 }
-
-export interface studentProfileDTO {
-  studentId: number;
-  studentName: string;
-  studentContact: string;
-  studentEmail: string;
-  studentGender: "MALE" | "FEMALE" | "OTHER";
-  studentSection: "A" | "B" | "C" | "D" | "E";
-  studentSemester: "2" | "4" | "6";
-  studentSession: "2027" | "2028" | "2029";
-  studentUniversityRollNo: string;
-}
