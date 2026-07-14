2f76860f87e347ac0e1b5a83f3dd336d22cf1eb3
Mukul <mukulkashyap.business@gmail.com>
2026-07-14 23:27:38 +0530

    make all the PATCH fields optional and remove the use of studentId sent by the client

--- src/lib/validations/student-input/studentInput.ts
+++ src/lib/validations/student-input/studentInput.ts
@@ -1,15 +1,14 @@
 import { z } from "zod";
 
 export const studentInputSchema = z.object({
-  studentId: z.number().int().positive(),
-  studentName: z.string().min(1),
-  studentUniversityRollNo: z.string().min(1),
-  studentEmail: z.string().min(1),
-  studentContact: z.string().min(1),
-  studentGender: z.enum(["MALE", "FEMALE", "OTHER"]),
-  studentSession: z.enum(["2027", "2028", "2029"]),
-  studentSemester: z.enum(["2", "4", "6"]),
-  studentSection: z.enum(["A", "B", "C", "D", "E"]),
+  studentName: z.string().min(1).optional(),
+  studentUniversityRollNo: z.string().min(1).optional(),
+  studentEmail: z.string().min(1).optional(),
+  studentContact: z.string().min(1).optional(),
+  studentGender: z.enum(["MALE", "FEMALE", "OTHER"]).optional(),
+  studentSession: z.enum(["2027", "2028", "2029"]).optional(),
+  studentSemester: z.enum(["2", "4", "6"]).optional(),
+  studentSection: z.enum(["A", "B", "C", "D", "E"]).optional(),
 });
 
 export type StudentInput = z.infer<typeof studentInputSchema>;
--- src/lib/validations/student-input/validateStudentRequest.ts
+++ src/lib/validations/student-input/validateStudentRequest.ts
@@ -1,10 +1,7 @@
 import { prisma } from "@/lib/prisma";
 import { studentInputSchema } from "./studentInput";
 
-export async function validateStudentRequest(
-  request: Request,
-  studentId: number,
-) {
+export async function validateStudentRequest(request: Request) {
   const body = await request.json();
   const result = studentInputSchema.safeParse(body);
 
@@ -13,33 +10,6 @@ export async function validateStudentRequest(
       success: false,
       status: 400,
       message: "Invalid Request",
-      data: null,
-    };
-  }
-
-  if (result.data.studentId !== studentId) {
-    return {
-      success: false,
-      message: "Invalid StudentId",
-      data: null,
-      status: 400,
-    };
-  }
-
-  const student = await prisma.student.findUnique({
-    where: {
-      studentId: result.data.studentId,
-    },
-    include: {
-      studentInternships: true,
-    },
-  });
-
-  if (!student) {
-    return {
-      success: false,
-      status: 404,
-      message: "Student not found",
       data: null,
     };
   }
